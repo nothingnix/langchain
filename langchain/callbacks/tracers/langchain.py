26d9a3e1210d860fc59af7a316de7581a27ccb3c
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-05-24 06:46:29 -0700

    Reduce number of calls

--- langchain/callbacks/tracers/langchain.py
+++ langchain/callbacks/tracers/langchain.py
@@ -8,7 +8,6 @@ from typing import Any, Dict, List, Optional
 from uuid import UUID
 
 import requests
-from tenacity import retry, stop_after_attempt, wait_fixed
 
 from langchain.callbacks.tracers.base import BaseTracer
 from langchain.callbacks.tracers.schemas import (
@@ -16,7 +15,6 @@ from langchain.callbacks.tracers.schemas import (
     RunCreate,
     RunTypeEnum,
     TracerSession,
-    TracerSessionCreate,
 )
 from langchain.schema import BaseMessage, messages_to_dict
 from langchain.utils import raise_for_status_with_text
@@ -34,33 +32,13 @@ def get_endpoint() -> str:
     return os.getenv("LANGCHAIN_ENDPOINT", "http://localhost:1984")
 
 
-@retry(stop=stop_after_attempt(3), wait=wait_fixed(0.5))
-def _get_tenant_id(
-    tenant_id: Optional[str], endpoint: Optional[str], headers: Optional[dict]
-) -> str:
-    """Get the tenant ID for the LangChain API."""
-    tenant_id_: Optional[str] = tenant_id or os.getenv("LANGCHAIN_TENANT_ID")
-    if tenant_id_:
-        return tenant_id_
-    endpoint_ = endpoint or get_endpoint()
-    headers_ = headers or get_headers()
-    response = requests.get(endpoint_ + "/tenants", headers=headers_)
-    raise_for_status_with_text(response)
-    tenants: List[Dict[str, Any]] = response.json()
-    if not tenants:
-        raise ValueError(f"No tenants found for URL {endpoint_}")
-    return tenants[0]["id"]
-
-
 class LangChainTracer(BaseTracer):
     """An implementation of the SharedTracer that POSTS to the langchain endpoint."""
 
     def __init__(
         self,
-        tenant_id: Optional[str] = None,
         example_id: Optional[UUID] = None,
         session_name: Optional[str] = None,
-        session_extra: Optional[Dict[str, Any]] = None,
         **kwargs: Any,
     ) -> None:
         """Initialize the LangChain tracer."""
@@ -68,10 +46,8 @@ class LangChainTracer(BaseTracer):
         self.session: Optional[TracerSession] = None
         self._endpoint = get_endpoint()
         self._headers = get_headers()
-        self.tenant_id = tenant_id
         self.example_id = example_id
         self.session_name = session_name or os.getenv("LANGCHAIN_SESSION", "default")
-        self.session_extra = session_extra
 
     def on_chat_model_start(
         self,
@@ -100,41 +76,14 @@ class LangChainTracer(BaseTracer):
         self._start_trace(chat_model_run)
         self._on_chat_model_start(chat_model_run)
 
-    def ensure_tenant_id(self) -> str:
-        """Load or use the tenant ID."""
-        tenant_id = self.tenant_id or _get_tenant_id(
-            self.tenant_id, self._endpoint, self._headers
-        )
-        self.tenant_id = tenant_id
-        return tenant_id
-
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(0.5))
-    def ensure_session(self) -> TracerSession:
-        """Upsert a session."""
-        if self.session is not None:
-            return self.session
-        tenant_id = self.ensure_tenant_id()
-        url = f"{self._endpoint}/sessions?upsert=true"
-        session_create = TracerSessionCreate(
-            name=self.session_name, extra=self.session_extra, tenant_id=tenant_id
-        )
-        r = requests.post(
-            url,
-            data=session_create.json(),
-            headers=self._headers,
-        )
-        raise_for_status_with_text(r)
-        self.session = TracerSession(**r.json())
-        return self.session
-
     def _persist_run_nested(self, run: Run) -> None:
         """Persist a run."""
-        session = self.ensure_session()
         child_runs = run.child_runs
         run_dict = run.dict()
         del run_dict["child_runs"]
-        run_create = RunCreate(**run_dict, session_id=session.id)
+        run_create = RunCreate(**run_dict, session_name=self.session_name)
         try:
+            # TODO: Add retries when async
             response = requests.post(
                 f"{self._endpoint}/runs",
                 data=run_create.json(),
